f8f5f801b8f4dae235a854dd5b220008ff2a8fab
zekturk <zk.senturk@gmail.com>
2021-01-08 21:01:17 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# join_ipynbs
+
+This shell script is used to create one .ipynb file from various number of .ipynb files. 
+
+It takes an argument that gives relativ or absolute adress of the folder including the ipynb files to be joined. 
+
+## Getting Started
+
+Open the command line where the script is and then run the join_ipynbs.sh script with a existing file path as an argument
+
+### Prerequisites
+
+Bash shell
+
+```
+Examples:
+
+> ./ipynb_combiner.sh to/my/file/relativ/path
+
+> ./ipynb_combiner.sh /to/my/file/absolut/path
+```
+
+## Authors
+
+* **Zekeriya ŞENTÜRK** 
+
+## License
+
+This project is licensed under the **MIT License** - see the [LICENSE](LICENSE) file for details
+
+
---
+++ join_ipynbs.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+ 
+# Creates a new ipynb file joining all the .pynb files
+# in a given folder
+# Author: Zekeriya SENTURK
+# github.com/zekturk
+
+# 1 argument
+# The folder adress that includes targeted .ipynb files
+# Usage: ./ipynb_combainer.sh /my/project/folder
+
+
+declare structure_1
+declare my_line
+declare my_line_with_comma
+declare structure_2
+declare my_comma=","
+declare -a MY_ARRAY
+declare -i num
+
+declare project_folder="${1}"
+declare created_file=""${project_folder}"/all.ipynb"
+
+[[ $1 ]] || { echo "Missing argument : Folder_Adress" >&2; exit 1; }
+
+if [[ -e ${created_file} ]]; then { echo "There is already an all.ipynb file! Rename or delete exsisting one" >&2; exit 1; }; fi
+
+printf -v structure_1 "{\n \"cells\": ["
+printf -v my_line "  {\n   \"cell_type\": \"markdown\",\n   \"metadata\": {\n    \"collapsed\": true\n   },\n   \"source\": [\n    \"---\"\n   ]\n  }"
+printf -v my_line_with_comma "  {\n   \"cell_type\": \"markdown\",\n   \"metadata\": {\n    \"collapsed\": true\n   },\n   \"source\": [\n    \"---\"\n   ]\n  },"
+
+MY_ARRAY=("$(ls "${project_folder}" | grep .ipynb)")
+[[ ${MY_ARRAY} ]] || { echo "There is ipynb file!" >&2; exit 1; }
+
+touch ${created_file}
+
+printf "${structure_1}" > ${created_file}
+
+num=0
+
+
+for file in ${MY_ARRAY}; do
+    if [[ num -eq 0 ]]; then
+        echo "${file}";
+        structure_2=$(cat ""${project_folder}"/"${file}"");
+        structure_2=${structure_2##*]};
+        ((++num))
+    fi
+    new_var=$(cat ""${project_folder}"/"${file}"") 
+    new_var=${new_var#*[}
+    new_var=${new_var%]*}
+    echo -n "${new_var}" >> ${created_file}
+    echo ${my_comma} >> ${created_file}
+    echo "${my_line_with_comma}" >> ${created_file}
+done
+
+echo "${my_line}" >> ${created_file}
+printf "]" >> ${created_file}
+echo "${structure_2}" >> ${created_file}
